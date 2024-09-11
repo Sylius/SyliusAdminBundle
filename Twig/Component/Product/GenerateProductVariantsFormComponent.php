2654623183eec400d9417653b2a3211fc064510a
Jan Goralski <jan.wojciech.goralski@gmail.com>
2024-09-11 17:32:45 +0200

    Extract a common ResourceFormElementTrait Create a basic ResourceFormComponent

--- Twig/Component/Product/GenerateProductVariantsFormComponent.php
+++ Twig/Component/Product/GenerateProductVariantsFormComponent.php
@@ -13,34 +13,17 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\AdminBundle\Twig\Component\Product;
 
-use Sylius\Component\Core\Model\Product;
-use Sylius\TwigHooks\LiveComponent\HookableLiveComponentTrait;
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\Form\FormInterface;
+use Sylius\Bundle\UiBundle\Twig\Component\ResourceFormComponentTrait;
+use Sylius\Component\Core\Model\ProductInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
-use Symfony\UX\LiveComponent\Attribute\LiveProp;
-use Symfony\UX\LiveComponent\DefaultActionTrait;
 use Symfony\UX\LiveComponent\LiveCollectionTrait;
 
 #[AsLiveComponent]
 class GenerateProductVariantsFormComponent
 {
-    use DefaultActionTrait;
-    use HookableLiveComponentTrait;
     use LiveCollectionTrait;
-
-    #[LiveProp(fieldName: 'resource')]
-    public ?Product $resource = null;
-
-    /** @param class-string $formClass */
-    public function __construct(
-        private readonly FormFactoryInterface $formFactory,
-        private readonly string $formClass,
-    ) {
-    }
-
-    protected function instantiateForm(): FormInterface
-    {
-        return $this->formFactory->create($this->formClass, $this->resource);
+    /** @use ResourceFormComponentTrait<ProductInterface> */
+    use ResourceFormComponentTrait {
+        initialize as public __construct;
     }
 }
