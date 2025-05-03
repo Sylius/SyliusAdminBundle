58f878387a92a2b893eccff449974643516ec8df
Jibé Barth <barth.jib@gmail.com>
2025-05-03 19:06:01 +0200

    feat(stimulus): Smooth upgrade

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const path = require('path');
 const Encore = require('@symfony/webpack-encore');
 
 class SyliusAdmin {
-    static getWebpackConfig(rootDir) {
+    /**
+     * Provide a light Webpack configuration for Sylius Admin
+     * All the stimulus stuff should be handled by the app.admin entrypoint
+     */
+    static getBaseWebpackConfig(rootDir) {
         this._prepareWebpackConfig(rootDir);
         Encore
             .addEntry('admin-entry', path.resolve(__dirname, 'Resources/assets/entrypoint.js'))
@@ -26,12 +30,18 @@ class SyliusAdmin {
         return adminConfig;
     }
 
-    static _getInternalWebpackConfig(rootDir) {
+    /**
+     * For a ready-to-use Stimulus bridge. Should be used only for sylius/sylius tests
+     * For instances started with Sylius-Standard < 2.0.4, it'll still be used unless upgrading webpack.config.js
+     * to use the method above getBaseWebpackConfig()
+     */
+    static getWebpackConfig(rootDir) {
         this._prepareWebpackConfig(rootDir);
-        // For a ready-to-use Stimulus bridge. Should be used only for sylius/sylius tests
+
         Encore
             .addEntry('admin-entry', path.resolve(__dirname, 'Resources/assets/app.js'))
             .enableStimulusBridge(path.resolve(__dirname, 'Resources/assets/controllers.json'));
+
         const adminConfig = Encore.getWebpackConfig();
 
         adminConfig.externals = { ...adminConfig.externals, window: 'window', document: 'document' };
