7f4f6eb53d9ac97fdf96f1cb7dd690c010387aba
Jibé Barth <barth.jib@gmail.com>
2025-05-03 18:29:39 +0200

    feat(stimulus): Switch to a bundle named namespace
    Require stimulus controller through controllers.json
    Do not add them by default on webpack config for both admin/shop

--- index.js
+++ index.js
@@ -12,20 +12,9 @@ const Encore = require('@symfony/webpack-encore');
 
 class SyliusAdmin {
     static getWebpackConfig(rootDir) {
+        this._prepareWebpackConfig(rootDir);
         Encore
-            .setOutputPath('public/build/admin/')
-            .setPublicPath('/build/admin')
             .addEntry('admin-entry', path.resolve(__dirname, 'Resources/assets/entrypoint.js'))
-            .addEntry('admin-product-entry', path.resolve(__dirname, 'Resources/assets/product-entrypoint.js'))
-            .disableSingleRuntimeChunk()
-            .cleanupOutputBeforeBuild()
-            .enableSourceMaps(!Encore.isProduction())
-            .enableVersioning(Encore.isProduction())
-            .enableSassLoader((options) => {
-                // eslint-disable-next-line no-param-reassign
-                options.additionalData = `$rootDir: '${rootDir}';`;
-            })
-            .enableStimulusBridge(path.resolve(__dirname, 'Resources/assets/controllers.json'));
 
         const adminConfig = Encore.getWebpackConfig();
 
@@ -35,6 +24,37 @@ class SyliusAdmin {
         Encore.reset();
 
         return adminConfig;
+    }
+
+    static _getInternalWebpackConfig(rootDir) {
+        this._prepareWebpackConfig(rootDir);
+        // For a ready-to-use Stimulus bridge. Should be used only for sylius/sylius tests
+        Encore
+            .addEntry('admin-entry', path.resolve(__dirname, 'Resources/assets/app.js'))
+            .enableStimulusBridge(path.resolve(__dirname, 'Resources/assets/controllers.json'));
+        const adminConfig = Encore.getWebpackConfig();
+
+        adminConfig.externals = { ...adminConfig.externals, window: 'window', document: 'document' };
+        adminConfig.name = 'admin';
+
+        Encore.reset();
+
+        return adminConfig;
+    }
+
+    static _prepareWebpackConfig(rootDir) {
+        Encore
+            .setOutputPath('public/build/admin/')
+            .setPublicPath('/build/admin')
+            .addEntry('admin-product-entry', path.resolve(__dirname, 'Resources/assets/product-entrypoint.js'))
+            .disableSingleRuntimeChunk()
+            .cleanupOutputBeforeBuild()
+            .enableSourceMaps(!Encore.isProduction())
+            .enableVersioning(Encore.isProduction())
+            .enableSassLoader((options) => {
+                // eslint-disable-next-line no-param-reassign
+                options.additionalData = `$rootDir: '${rootDir}';`;
+            })
     }
 }
 
